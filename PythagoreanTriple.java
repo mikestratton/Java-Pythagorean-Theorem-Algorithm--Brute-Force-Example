6504746aa32cb3356a87df4266f907965f6ca67d
Mike Stratton <mike@mikestratton.net>
2020-07-03 15:45:36 -0400

    Update PythagoreanTriple.java

--- PythagoreanTriple.java
+++ PythagoreanTriple.java
@@ -1,25 +1,22 @@
 import java.util.Scanner; // initialize scanner
+
 public class PythagoreanTriple
 {
-public static void main( String[] args )
-{
-
-for ( int a = 1; a <= 500; a++ )
-{
-for ( int b = 1; b <= 500; b++ )
-{
-for ( int c = 1; c <= 500; c++ )
-{
-
-if( (a * a) + (b * b) == (c * c) )
-{
-System.out.printf( “%d, %d, %d”, a, b, c );
-System.out.println();
-} // end if
-} // end 3rd loop inside of 2nd loop
-} // end 2nd loop inside of first loop
-} // end first loop
-
-} // end method main
-
+  public static void main( String[] args )
+  {
+    for ( int a = 1; a <= 500; a++ )
+    {
+     for ( int b = 1; b <= 500; b++ )
+     {
+        for ( int c = 1; c <= 500; c++ )
+        {
+          if( (a * a) + (b * b) == (c * c) )
+          {
+           System.out.printf( “%d, %d, %d”, a, b, c );
+           System.out.println();
+          } // end if
+        } // end 3rd loop inside of 2nd loop
+      } // end 2nd loop inside of first loop
+    } // end first loop
+  } // end method main
 } // end class PythagoreanTriple
